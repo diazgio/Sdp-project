cfcdf117e257e949e9c7fbe8b0d33400e3d540ad
Sanad <s_wj_88@hotmail.com>
2020-11-20 20:57:55 +0200

    Configure the mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,18 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "example.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "semanaelapublicidad@gmail.com",
-      password: "Gio123456"
-  }
-
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
