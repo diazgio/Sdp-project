39d0710c5607bc28e0432a5d9b1b2a5abb45bf70
Sanad <s_wj_88@hotmail.com>
2020-11-20 20:04:45 +0200

    Configure the mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "example.com",
+      domain: "gmail.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: "semanaelapublicidad@gmail.com",
       password: "Gio123456"
   }
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://tranquil-garden-10221.herokuapp.com' }
 end
