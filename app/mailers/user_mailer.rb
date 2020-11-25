f6ea47388bb0c9c76183289e045ba4495ba001cb
Sanad <s_wj_88@hotmail.com>
2020-11-25 22:22:35 +0200

    Add images to the email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,15 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
+    data = File.read(Rails.root.join('public/sdpfucsia.png'))
+    lock = File.read(Rails.root.join('public/white.png'))
+    logos = File.read(Rails.root.join('public/logos.png'))
+    blogos = File.read(Rails.root.join('public/blogos.png'))
+    attachments.inline['sdpfucsia.png'] = data
+    attachments.inline['white.png'] = lock
+    attachments.inline['logos.png'] = logos
+    attachments.inline['blogos.png'] = blogos
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Bienvenidos a la Semana de la Publicidad 2020 ')
   end
 end
