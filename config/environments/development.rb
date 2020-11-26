499cab3e7bc840ae6dd43f473c23b5a11c0e862e
Giordano Dìaz <diazgiordano@gmail.com>
2020-11-26 11:46:08 -0500

    Add gmail to domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "mydomain.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "informessdp@gmail.com",
