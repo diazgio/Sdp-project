be01fc9074fa318837b1b725246866a661ab1015
Sanad <s_wj_88@hotmail.com>
2020-11-27 17:27:58 +0200

    Change the mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,16 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    data = File.read(Rails.root.join('public/sdpfucsia.png'))
-    lock = File.read(Rails.root.join('public/white.png'))
-    logos = File.read(Rails.root.join('public/logos.png'))
-    blogos = File.read(Rails.root.join('public/logosblack.jpeg'))
-    attachments.inline['sdpfucsia.png'] = data
-    attachments.inline['white.png'] = lock
-    attachments.inline['logos.png'] = logos
-    attachments.inline['logosblack.jpeg'] = blogos
+    # data = File.read(Rails.root.join('public/sdpfucsia.png'))
+    # lock = File.read(Rails.root.join('public/white.png'))
+    # logos = File.read(Rails.root.join('public/logos.png'))
+    # blogos = File.read(Rails.root.join('public/logosblack.jpeg'))
+    # attachments.inline['sdpfucsia.png'] = data
+    # attachments.inline['white.png'] = lock
+    # attachments.inline['logos.png'] = logos
+    # attachments.inline['logosblack.jpeg'] = blogos
+    sdbmail = File.read(Rails.root.join('public/sdbmail.png'))
+    attachments.inline['sdbmail.png'] = sdbmail
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Bienvenidos a la Semana de la Publicidad 2020 ')
   end
