b8693e7363bef832a82c9e7de8d67bc56592343d
Giordano Dìaz <diazgiordano@gmail.com>
2020-11-30 17:44:35 -0500

    Modifing lenght of dni

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, :last_name, :dni, :email, :position, :author,:company, :city, presence: true
-  validates :dni, length: {minimum:4, maximum:10}
+  validates :dni, length: { minimum:4 }
 end
